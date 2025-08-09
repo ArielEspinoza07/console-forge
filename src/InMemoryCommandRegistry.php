106e9f5af3088bf15e404087efc8cbbaa5526a6f
Ariel Espinoza <arielespinoza46@gmail.com>
2025-08-08 22:36:41 -0600

    feat: add in memory registry

---
+++ src/InMemoryCommandRegistry.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ConsoleForge;
+
+use ConsoleForge\Contracts\CommandDescriptorInterface;
+use ConsoleForge\Contracts\CommandRegistryInterface;
+use InvalidArgumentException;
+
+final class InMemoryCommandRegistry implements CommandRegistryInterface
+{
+    /**
+     * @var array<string, CommandDescriptorInterface>
+     */
+    private array $commands = [];
+
+    public function add(CommandDescriptorInterface $descriptor): CommandRegistryInterface
+    {
+        $name = $descriptor->name();
+
+        if (isset($this->commands[$name])) {
+            throw new InvalidArgumentException(
+                sprintf("A command with the name '%s' is already registered.", $name)
+            );
+        }
+
+        $this->commands[$name] = $descriptor;
+
+        return $this;
+    }
+
+    public function get(string $name): CommandDescriptorInterface
+    {
+        if (! isset($this->commands[$name])) {
+            throw new InvalidArgumentException(
+                sprintf("No command registered with the name '%s'.", $name)
+            );
+        }
+
+        return $this->commands[$name];
+    }
+
+    /**
+     * @return iterable<CommandDescriptorInterface>
+     */
+    public function all(): iterable
+    {
+        return array_values($this->commands);
+    }
+
+    public function has(string $name): bool
+    {
+        return isset($this->commands[$name]);
+    }
+
+    public function remove(string $name): void
+    {
+        unset($this->commands[$name]);
+    }
+
+    public function clear(): void
+    {
+        $this->commands = [];
+    }
+}
